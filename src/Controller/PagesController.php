f9faeb1a5d925475c615efcf57f7869741c28ad9
nowoszko <30315346+nowoszko@users.noreply.github.com>
2017-08-24 18:58:06 +0200

    a

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -134,6 +134,7 @@ $mailsy['created_at']=''.DATE('Y-m-d H:i:s', strtotime(''.$str['created_at'].'')
 $mailsy['created']=time();
 $mailsy['body']=''.txtToJson($str['body']).'';
 $mailsy['active']=1;
+print_r($str);
 array_push($mails, $mailsy);
 }
    }
