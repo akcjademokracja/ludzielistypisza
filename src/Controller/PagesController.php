51aa808687a5a7036a6d5b3ca544676ba40413ef
nowoszko <30315346+nowoszko@users.noreply.github.com>
2017-08-24 18:37:32 +0200

    a

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -131,7 +131,8 @@ $mailsy['subject']=''.addslashes($str['subject']).'';
 $mailsy['firstname']=''.$str['firstname'].'';
 $mailsy['created_at']=''.DATE('Y-m-d H:i:s', strtotime(''.$str['created_at'].'')).'';
 $mailsy['created']=time();
- $mailsy['active']=1;
+$mailsy['body']='''';
+$mailsy['active']=1;
 array_push($mails, $mailsy);
    }
     	  	$addFeed = $this->mailsTable->newEntities($mails);
